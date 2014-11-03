304255e8a917c7f4fa3b25b007ddfab7f986958d
brent-g <brent.godsoe@gmail.com>
2014-11-03 01:10:14 -0500

    Fade in text

--- application/views/message.php
+++ application/views/message.php
@@ -1,19 +1,38 @@
 <script>
 	var msg = new SpeechSynthesisUtterance('<?php echo($message);?>');
-	var voices = speechSynthesis.getVoices();
+	//var voices = speechSynthesis.getVoices();
    	window.speechSynthesis.speak(msg);
-   	console.log('voices', voices);
+
+   	$(function(){
+	   	var $el = $("#message:first");
+	   	var text = $.trim($el.text());
+	    // split the words by spaces
+	    var words = text.split(" "), html = "";
+
+		for (var i = 0; i < words.length; i++) {
+		    html += "<span>" + words[i] + ((i+1) === words.length ? "" : " ") + "</span>";
+		};
+		$el.html(html).children().hide().each(function(i){
+		  $(this).delay(i*200).fadeIn(900);
+		});
+		$el.find("span").promise().done(function(){
+		    $el.text(function(i, text){
+		       return $.trim(text);
+		    });            
+		});
+	});
 </script>
 <style>
 #message{
-	width:300px;
+	width:400px;
     height:auto;
     position:absolute;
     left:50%;
     top:50%;
     margin:-100px 0 0 -150px;
     text-align: center;
+    font-size: 28px;
 }
-
 </style>
-    <div id="message"><?php echo($message);?></div>
+
+<div id="message"><?php echo($message);?></div>
