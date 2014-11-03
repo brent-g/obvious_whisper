dc367d4ea71ab23f9f89682ea311068e9d9fc17c
brent-g <brent.godsoe@gmail.com>
2014-11-03 17:04:56 -0500

    This is only a test

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -1,6 +1,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
-	<title>Obvious Whisper</title>
+	<title>Obvious Whisper 123</title>
 	<link href='http://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
 	<link href="<?php echo site_url('assets'); ?>/css/main.css" rel="stylesheet" type="text/css">
 	<script type="text/javascript" src="<?php echo site_url('assets'); ?>/js/jquery.js"></script>
