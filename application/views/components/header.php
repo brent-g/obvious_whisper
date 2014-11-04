d8faa94c6796f02c040d633dc8e4e42bd88e512b
brent-g <brent.godsoe@gmail.com>
2014-11-04 00:15:05 -0500

    HTML Validation + Meta Info

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -1,6 +1,12 @@
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title>Obvious Whisper</title>
+		<meta name="description" content="Verbalize a thought from the comfort of text">
+		<meta name="keywords" content="obvious, whisper, anonymous, send, message, talk, text, speech">
+		<meta name="author" content="Brent Godsoe">
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link href='http://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css' />
 		<link href="<?php echo site_url('assets'); ?>/css/main.css" rel="stylesheet" type="text/css" />
 		<script type="text/javascript" src="<?php echo site_url('assets'); ?>/js/jquery.js"></script>
