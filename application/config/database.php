dc3c581ab4d3bd3b4e86c6b359faadecc1c182b8
brent_g <brent.godsoe@gmail.com>
2014-11-04 01:56:31 +0000

    config fix

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'HippoPoliceAquaTeen';
 $db['default']['database'] = 'obvious_whisper';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
